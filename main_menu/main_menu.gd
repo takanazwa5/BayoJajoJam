26c1c2ab44e8f28878d2f3962deb4b122be300d8
takanazwa5 <tomasz.pawelczyk19@gmail.com>
2025-04-27 14:02:07 +0200

    main menu update

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -2,14 +2,28 @@ class_name MainMenu extends Control
 
 
 @onready var play_button: TextureButton = %PlayButton
+@onready var credits_button: Button = %CreditsButton
+@onready var how_to_play_button: Button = %HowToPlayButton
 
 
 func _ready() -> void:
 
 	play_button.pressed.connect(_on_play_button_pressed)
+	credits_button.pressed.connect(_on_credits_button_pressed)
+	how_to_play_button.pressed.connect(_on_how_to_play_button_pressed)
 
 
 func _on_play_button_pressed() -> void:
 
 	var main_scene: PackedScene = load("uid://77go706q5sb7")
 	get_tree().change_scene_to_packed(main_scene)
+
+
+func _on_credits_button_pressed() -> void:
+
+	pass
+
+
+func _on_how_to_play_button_pressed() -> void:
+
+	pass
